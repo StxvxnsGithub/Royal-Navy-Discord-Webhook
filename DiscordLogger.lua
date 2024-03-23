3d8f1c88cf1417b9bece511539cdef2f2127e1dd
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2024-03-23 00:50:20 +0000

    Version 0.11

--- DiscordLogger.lua
+++ DiscordLogger.lua
@@ -1,5 +1,5 @@
 -- Discord Logger
-local versionNo = "0.10"
+local versionNo = "0.11"
 
 ----------------------------------------------
 
@@ -10,6 +10,7 @@ local RunService = game:GetService("RunService")
 local HttpService = game:GetService("HttpService")
 local config = require(script.Config)
 local url = config.url
+local err = config.err
 local ownerPing = "<@" .. config.owner .. ">"
 
 -- Settings
@@ -35,9 +36,42 @@ local success, info = pcall(function()
 	placeID = game.PlaceId
 	return game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
 end)
-
 if success and info  then
 	placeName = info.Name
+end
+
+function discord.error(errorFields)
+	local fields = errorFields
+	table.insert(fields,
+		{
+			["name"] = "",
+			["value"] = "Version " .. versionNo .. " at [".. placeName .. "](https://www.roblox.com/games/" .. placeID ..")",
+			["inline"] = false
+		}
+	)
+	local body = {
+		["content"] = ownerPing,
+		["embeds"] = {
+			{
+				["color"] = 13908037,
+				["fields"] = fields,
+				["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%S." .. math.round(tick() % 1 * 1000) .. "Z")
+			}
+		}
+	}
+	
+	local success, response = pcall(function()
+		return HttpService:PostAsync(err, HttpService:JSONEncode(body)) -- Uses the webhook for errors
+	end)
+	if success then
+		print("DISCORD LOGGER: error logged.")
+		return true
+	else
+		local responseBody = response.Body:ReadAsStringAsync()
+		responseBody:GetAwaitResult()
+		warn("DISCORD LOGGER: failed to log error. ERROR: " .. response .. "; SECONDARY ERROR: " .. responseBody)
+	end
+	return false
 end
 
 function discord.send()
@@ -76,29 +110,31 @@ function discord.send()
 				local responseBody = response.Body:ReadAsStringAsync()
 				responseBody:GetAwaitResult()
 				errResponse, errBody = response, responseBody
+				
 				warn("DISCORD LOGGER: failed to send messages. ERROR: " .. response .. " " .. responseBody)
 				print("DISCORD LOGGER: attempting error logging...")
 				
-				-- As error may be due to a data format error, attempts a simple error log
-				task.delay(30, function()
-					local secondarySuccess, secondaryResponse = pcall(function() 
-						return HttpService:PostAsync(url, HttpService:JSONEncode({content = ownerPing .. " **ERROR:** message send failed. " .. response .. " " .. responseBody}))
-					end)
-					if secondarySuccess then
-						print("DISCORD LOGGER: error logged.")
-					else
-						local secondaryResponseBody = response.Body:ReadAsStringAsync()
-						secondaryResponseBody:GetAwaitResult()
-						warn("DISCORD LOGGER: failed to log error. ERROR: " .. secondaryResponse .. "; SECONDARY ERROR: " .. secondaryResponseBody)
-					end
+				-- As error may be due to a data format error, attempts an error log to the backup
+				local success, response = pcall(function()
+					return discord.error({
+						{["name"] = "ERROR OCCURED", ["value"] = "Message send failed.", ["inline"] = false},
+						{["name"] = "ERROR DETAILS", ["value"] = errResponse .. " " .. errBody, ["inline"] = false}
+					})
 				end)
+				if success then
+					print("DISCORD LOGGER: error logged.")
+					return true
+				end
 			end
 		elseif failCount == maxFails then -- Should only attempt logging a failure to the channel once
 			failCount += 1
 			warn("DISCORD LOGGER: failed to send messages. ERROR: send failed " .. (failCount-1) ..  " times. ACTION: halting script.")
-			task.delay(10, function()
-				HttpService:PostAsync(url, HttpService:JSONEncode({content = ownerPing .. " **ERROR:** message sending failed " .. (failCount-1) ..  " times. **ACTION:** halting script."}))
-				
+
+			local success, response = pcall(function()
+				return discord.error({
+					{["name"] = "ERROR OCCURED", ["value"] = "Failed to send messages " .. (failCount-1) ..  " times.", ["inline"] = false},
+					{["name"] = "ERROR ACTIONS", ["value"] = "Halted script.", ["inline"] = false}
+				})
 			end)
 		else
 			warn("DISCORD LOGGER: halted due to errors. ERROR: " .. errResponse .. " " .. errBody)
@@ -112,7 +148,7 @@ end
 
 function discord.queue(method, message)
 	if enabled then
-		table.insert(queue, message) -- Add player to the list
+		table.insert(queue, message)
 		
 		if not sending then
 			sending = true
@@ -122,26 +158,25 @@ function discord.queue(method, message)
 				if sending then
 					sending = false
 					print("DISCORD LOGGER: messages sending.")
+					
 					local success, response = pcall(discord.send)
-
 					if not success then
 						warn("DISCORD LOGGER: failed to send. ERROR: " .. response)
 						discord.queue("error", response)
 					end
 				end
-
-				--sending = false
 			end)
 		elseif #queue >= queueSize then
 			sending = false
 			warn("DISCORD LOGGER: forcing send due to queue capacity reached.")
 			
-			local queuedMessage = {
-				["name"] = "FORCED SEND",
-				["value"] = "Message queue capacity reached",
-				["inline"] = false
-			}
-			table.insert(queue, queuedMessage)
+			table.insert(queue, 
+				{
+					["name"] = "FORCED SEND",
+					["value"] = "Message queue capacity reached",
+					["inline"] = false
+				}
+			)
 			
 			local success, response = pcall(discord.send)
 			if not success then
@@ -154,23 +189,6 @@ function discord.queue(method, message)
 	else
 		warn("DISCORD LOGGER: not sent due to studio test server. " .. message.value)
 	end
-	
-	return true
-end
-
-function discord.shutdown()
-	local embedMessage = {
-		["name"] = "SERVER SHUTDOWN",
-		["value"] = "Remaining queued messages logged",
-		["inline"] = false
-	}
-	table.insert(queue, embedMessage)
-	
-	local success, response = pcall(discord.send)
-	if not success then
-		warn("DISCORD LOGGER: failed to send remaining messages when shutting down. ERROR: " .. response)
-		return false
-	end
 	return true
 end
 
@@ -180,11 +198,11 @@ function discord.basicSend(message)
 		["value"] = message,
 		["inline"] = false
 	}
+	
 	if discord.queue("basic", embedMessage) then
 		return true
 	end
-
-	warn("DISCORD LOGGER: basicSend failure")
+	warn("DISCORD LOGGER: basicSend failure.")
 	return false
 end
 
@@ -196,7 +214,7 @@ function discord.playerSend(player, message)
 	if displayName == playerName then
 		displayName = ""
 	else
-		displayName = " \"" .. displayName .. "\""
+		displayName = " \"" .. displayName .. "\"" -- Output: "displayName"
 	end
 	
 	local playerMessage = "[" .. playerName .. displayName .. " (" .. playerID .. ")](https://www.roblox.com/users/" .. playerID .. ") " .. message
@@ -205,12 +223,31 @@ function discord.playerSend(player, message)
 		["value"] = playerMessage,
 		["inline"] = false
 	}
+	
 	if discord.queue("player", embedMessage) then
 		return true
 	end
-	
-	warn("DISCORD LOGGER: playerSend failure")
+	warn("DISCORD LOGGER: playerSend failure.")
 	return false
+end
+
+function discord.shutdown()
+	if enabled then
+		table.insert(queue, 
+			{
+				["name"] = "SERVER SHUTDOWN",
+				["value"] = "Remaining queued messages logged",
+				["inline"] = false
+			}
+		)
+
+		local success, response = pcall(discord.send)
+		if not success then
+			warn("DISCORD LOGGER: failed to send remaining messages when shutting down. ERROR: " .. response)
+			return false
+		end
+	end
+	return true
 end
 
 return discord
